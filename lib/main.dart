edc60e14bd2be967b8d646f8b8c63c86666df603
sawitree <6631501112@lamduan.mfu.ac.th>
2026-03-26 22:10:42 +0700

    save work

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'signin01.dart';
-
+import 'updates_account_user.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Signin01(),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-    ),
-  );
+      home: SuccessScreen(),
+    );
+  }
 }
