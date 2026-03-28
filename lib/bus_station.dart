422fbb3013a80cc76d0fcc21e75ed903bfa8105d
sawitree <6631501112@lamduan.mfu.ac.th>
2026-03-29 01:05:28 +0700

    ฺ๊BUS

---
+++ lib/bus_station.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class BusStationPage extends StatelessWidget {
+  const BusStationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "MFU Transit",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(6),
+                      decoration: BoxDecoration(
+                      ),
+                      child: const Icon(Icons.close),
+                    ),
+                  )
+                ],
+              ),
+
+              const SizedBox(height: 20),
+
+              buildRouteButton("สาย 1"),
+              const SizedBox(height: 12),
+              buildRouteButton("สาย 2 (โรงพยาบาลแม่ฟ้าหลวง)"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildRouteButton(String text) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        children: [
+          const Icon(Icons.add_circle_outline, color: Colors.white),
+          const SizedBox(width: 10),
+          Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
